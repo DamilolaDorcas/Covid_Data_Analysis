b58baec516f529a7f46addfa71432b534c9e323e
Damilola <dhurcasdamilola15@gmail.com>
2022-01-19 16:41:23 +0100

    countries with the highest death on 27th and 31st of January

--- covid19 query.sql
+++ covid19 query.sql
@@ -21,10 +21,24 @@ select confirmed, deaths, recovered, countryregion from covid_19_data
 where countryregion LIKE 'Mainland China'
 
 --Retrieve information for the top 5 countries with the highest confirmed cases.
-SELECT  SUM(onfirmed) AS Top_Confirmed, countryregion
+SELECT  SUM(confirmed) AS Top_Confirmed, countryregion
 FROM covid_19_data
 GROUP BY countryregion
 ORDER BY SUM(confirmed) desc
 LIMIT  5 
+
+
+--Retrieve information for the top 5 countries with the highest death cases specifically for 27th and 31st of January, 2020.
+select countryregion,observationdate, SUM(deaths) AS Top_Deaths from covid_19_data 
+WHERE observationdate = '2020-01-31' 
+Group by countryregion, observationdate
+ORDER BY SUM(deaths)DESC
+
+
+select countryregion,observationdate, SUM(deaths) AS Top_Deaths from covid_19_data 
+WHERE observationdate = '2020-01-27' 
+Group by countryregion, observationdate
+ORDER BY SUM(deaths)DESC
+
 
 
